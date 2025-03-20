5cd591b4972020f73ebca65db33dfa2cdd4bc8bc
Mario <airbomberofaqua@gmail.com>
2025-03-20 14:51:44 -0400

    trash

--- app/README.md
+++ app/README.md
@@ -10,3 +10,10 @@ Currently, two official plugins are available:
 ## Expanding the ESLint configuration
 
 If you are developing a production application, we recommend using TypeScript and enable type-aware lint rules. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+
+The README file describes the purpose and audience for the application
+
+We wanted to see a small sample of how proffesional level companies would make their news sites, and try it on our own (free way of course)
+
+The README file describes the user stories
+The README file include screenshots of the final project
--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,17 +1,24 @@
 import News from "./components/news";
 import WordGame from "./components/word";
 import Weather from "./components/weather";
-import "./App.css"; // Make sure you have a CSS file for styling
+import "./App.css";
 
 function App() {
   return (
     <div className="app-container">
-      <div className="main-content">
-        <Weather />
-        <WordGame />
-      </div>
-      <div className="news-container">
-        <News />
+      <h1 className="title">The Marcy Times</h1>
+      <div className="layout-container">
+        <div className="left-section">
+          <div className="news-container">
+            <News />
+          </div>
+          <div className="word-container">
+            <WordGame />
+          </div>
+        </div>
+        <div className="right-section">
+          <Weather />
+        </div>
       </div>
     </div>
   );
