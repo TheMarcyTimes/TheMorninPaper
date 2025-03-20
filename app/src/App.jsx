be2ac90d5c8d7244716e38044ab33ae595c003c7
Azim <annayev.azim@gmail.com>
2025-03-19 20:16:16 -0400

    implemented local storage and added to the app

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import News from "./components/news";
 import WordGame from "./components/word";
+import Weather from "./components/weather";
 
 function App() {
   const [showWordGame, setShowWordGame] = useState(false);
 
   return (
     <div>
+      <Weather />
       <button onClick={() => setShowWordGame(!showWordGame)}>
         {showWordGame ? "Show News" : "Play Word Game"}
       </button>
