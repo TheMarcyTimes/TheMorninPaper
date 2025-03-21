6a794771537146b83bbd9fd753bf81f78b2d4945
Azim <annayev.azim@gmail.com>
2025-03-21 16:44:10 -0400

    implemented the nav bar

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,22 +1,60 @@
+import React from "react";
 import News from "./components/news";
 import WordGame from "./components/word";
 import Weather from "./components/weather";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="app-container">
-      <h1 className="title">The Morning Paper</h1> {/* Centered title */}
-      <div className="content-wrapper">
-        <div className="news-wrapper">
-          <News />
+      {/* Stylish Navbar */}
+      <nav className="navbar navbar-expand-lg custom-navbar">
+        <div className="container-fluid">
+          <Link className="navbar-brand custom-brand" to="/">
+            🗞️ The Morning Paper
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link custom-link" to="/">
+                  News
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link custom-link" to="/wordgame">
+                  Word Game
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link custom-link" to="/weather">
+                  Weather
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="word-wrapper">
-          <WordGame />
-        </div>
-        <div className="weather-wrapper">
-          <Weather />
-        </div>
+      </nav>
+
+      {/* Page Content */}
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<News />} />
+          <Route path="/wordgame" element={<WordGame />} />
+          <Route path="/weather" element={<Weather />} />
+        </Routes>
       </div>
     </div>
   );
